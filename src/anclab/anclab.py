12579b3386e119b13ab79c0ec9e57d0dac654c53
cksajil <sajilck@gmail.com>
2023-02-12 20:40:16 +0530

    Update anclab module  On branch master  Changes to be committed: 	new file:   src/anclab/anclab.py 	deleted:    src/anclab/main.py

---
+++ src/anclab/anclab.py
@@ -0,0 +1,12 @@
+import numpy as np
+from helper import spectral_flatness
+
+
+def main():
+    signal = np.array(([1, 2, 3, 4, 5]))
+    output = spectral_flatness(signal, sampling_freq=8000)
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
