b66591efc6d54c0acd5c7f625486ed9a02a7b631
cksajil <sajilck@gmail.com>
2023-02-13 20:46:52 +0530

    Added basic plot class  On branch master  Changes to be committed: 	modified:   src/anclab/anclab.py 	new file:   src/ancplotter/ancplotter.py

--- src/anclab/anclab.py
+++ src/anclab/anclab.py
@@ -6,7 +6,7 @@ def main():
     signal = np.array(([1, 2, 3, 4, 5]))
     output_1 = spectral_flatness(signal, sampling_freq=8000)
     print(output_1)
-    
+
     output_2 = generate_signal(5, 16000)
     print(output_2)
 
---
+++ src/ancplotter/ancplotter.py
@@ -0,0 +1,4 @@
+class Plot:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
