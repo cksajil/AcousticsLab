3f994354183c2b540972edf671d57d5d0e0cb055
cksajil <sajilck@gmail.com>
2023-02-13 23:38:18 +0530

    Organise folder structure  On branch master  Changes to be committed: 	renamed:    src/anclab/__init__.py -> anclab/__init__.py 	renamed:    src/anclab/anclab.py -> anclab/anclab.py 	renamed:    src/anclab/helper.py -> anclab/helper.py 	renamed:    src/ancplotter/__init__.py -> ancplotter/__init__.py 	renamed:    src/ancplotter/ancplotter.py -> ancplotter/ancplotter.py 	new file:   main.py

--- ancplotter/ancplotter.py
+++ ancplotter/ancplotter.py
@@ -2,3 +2,6 @@ class Plot:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+
+    def print_size(self):
+        print(self.width, self.height)
---
+++ main.py
@@ -0,0 +1,5 @@
+from ancplotter.ancplotter import Plot
+
+
+graph_1 = Plot(6, 4)
+graph_1.print_size()
