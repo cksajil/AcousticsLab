c41491114d6954f85d59046ad8a52eb566773740
cksajil <>
2023-02-13 16:17:22 +0530

    Added function for test signal generation  On branch master  Changes to be committed: 	modified:   src/anclab/anclab.py 	modified:   src/anclab/helper.py

--- src/anclab/anclab.py
+++ src/anclab/anclab.py
@@ -1,11 +1,13 @@
 import numpy as np
-from helper import spectral_flatness
+from helper import spectral_flatness, generate_signal
 
 
 def main():
     signal = np.array(([1, 2, 3, 4, 5]))
-    output = spectral_flatness(signal, sampling_freq=8000)
-    print(output)
+    output_1 = spectral_flatness(signal, sampling_freq=8000)
+    print(output_1)
+    output_2 = generate_signal(5, 16000)
+    print(output_2)
 
 
 if __name__ == "__main__":
--- src/anclab/helper.py
+++ src/anclab/helper.py
@@ -9,3 +9,19 @@ def spectral_flatness(data, sampling_freq, offset=1e-20):
     freq, power = signal.periodogram(data, sampling_freq)
     spectralflatness = np.exp(np.mean(np.log(power+offset)))/np.mean(power)
     return spectralflatness
+
+
+def generate_signal(T, Fs):
+    """
+    Function to generate test signal
+    """
+    n = np.arange(1, T+1)
+    wsin = np.array((0.03, 0.06, 0.09))
+    ar = np.array((2, 1, 0.5))
+    br = np.array((-1, -0.5, 0.1))
+    narrow = np.zeros((1, T))
+    for k in range(0, len(wsin)):
+        narrow += ar[k]*np.cos(np.pi*wsin[k]*n)+br[k]*np.sin(np.pi*wsin[k]*n)
+    wide = np.sqrt(0.01)*np.random.randn(1, T)
+    x = narrow+wide
+    return x
