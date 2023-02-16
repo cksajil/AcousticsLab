6305b6d9218124956e302ca3d1c121484d9ac4ae
cksajil <sajilck@gmail.com>
2023-02-16 07:48:09 +0530

    Updated readme  On branch master  Changes to be committed: 	modified:   README.md 	modified:   main.py

--- README.md
+++ README.md
@@ -36,5 +36,36 @@ python -m pip install -r requirements.txt
 
 ### How to run
 ```console
-python main.py
+import numpy as np
+from ancplotter.ancplotter import Plot
+import pyroomacoustics as pra
+
+# Set room parameters
+rt60_tgt = 0.3
+room_dim = [10, 7.5, 3.5]
+e_absorption, max_order = pra.inverse_sabine(rt60_tgt,
+                                             room_dim)
+
+room = pra.ShoeBox(room_dim,
+                   fs=8000,
+                   materials=pra.Material(e_absorption),
+                   max_order=max_order)
+
+room.add_source([2.5, 3.73, 1.76],
+                signal=data,
+                delay=0.5)
+
+room.add_source([1.5, 4, 2],
+                signal=data,
+                delay=0.5)
+
+mic_locs = np.c_[[6.3, 4.87, 1.2]]
+
+room.add_microphone_array(mic_locs)
+
+#Simulate
+room.simulate()
+
+graph_1.secondary_path_graphs(room.rir[0][0],
+                              room.rir[0][1])
 ```
--- main.py
+++ main.py
@@ -1,34 +1,7 @@
 import numpy as np
 from ancplotter.ancplotter import Plot
-import pyroomacoustics as pra
 
 graph_1 = Plot(plot_type='rir_time', width=6)
 time = np.linspace(0., 1.0, num=1000)
 data = np.sin(2*np.pi*1000*time)
 # graph_1.rir_plot(data, 1000)
-
-rt60_tgt = 0.3
-room_dim = [10, 7.5, 3.5]
-e_absorption, max_order = pra.inverse_sabine(rt60_tgt,
-                                             room_dim)
-
-room = pra.ShoeBox(room_dim,
-                   fs=8000,
-                   materials=pra.Material(e_absorption),
-                   max_order=max_order)
-
-room.add_source([2.5, 3.73, 1.76],
-                signal=data,
-                delay=0.5)
-
-room.add_source([1.5, 4, 2],
-                signal=data,
-                delay=0.5)
-
-mic_locs = np.c_[[6.3, 4.87, 1.2]]
-
-room.add_microphone_array(mic_locs)
-room.simulate()
-
-graph_1.secondary_path_graphs(room.rir[0][0],
-                              room.rir[0][1])
