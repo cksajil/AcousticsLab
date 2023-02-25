c7714551fee670ee4eb085f4f1db33653c9cff47
cksajil <sajilck@gmail.com>
2023-02-25 10:10:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from anclab.fxlms import perform_fxlms
 from anclab.helper import generate_signal
 
 
-graph_1 = Plot(plot_type='rir_time', width=6)
-time = np.linspace(0., 1.0, num=1000)
-data = np.sin(2*np.pi*1000*time)
+graph_1 = Plot(plot_type="rir_time", width=6)
+time = np.linspace(0.0, 1.0, num=1000)
+data = np.sin(2 * np.pi * 1000 * time)
 # graph_1.rir_plot(data, 1000)
 
 in_signal = generate_signal(T=1000, Fs=8000)
@@ -15,21 +15,15 @@ in_signal = generate_signal(T=1000, Fs=8000)
 # Set room parameters
 rt60_tgt = 0.3
 room_dim = [10, 7.5, 3.5]
-e_absorption, max_order = pra.inverse_sabine(rt60_tgt,
-                                             room_dim)
+e_absorption, max_order = pra.inverse_sabine(rt60_tgt, room_dim)
 
-room = pra.ShoeBox(room_dim,
-                   fs=8000,
-                   materials=pra.Material(e_absorption),
-                   max_order=max_order)
+room = pra.ShoeBox(
+    room_dim, fs=8000, materials=pra.Material(e_absorption), max_order=max_order
+)
 
-room.add_source([2.5, 3.73, 1.76],
-                signal=data,
-                delay=0.5)
+room.add_source([2.5, 3.73, 1.76], signal=data, delay=0.5)
 
-room.add_source([1.5, 4, 2],
-                signal=data,
-                delay=0.5)
+room.add_source([1.5, 4, 2], signal=data, delay=0.5)
 
 mic_locs = np.c_[[6.3, 4.87, 1.2]]
 
